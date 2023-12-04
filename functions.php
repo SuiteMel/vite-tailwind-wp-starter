c7ee0a0f63d6994ab43775ec55a21398ecc9363d
SuiteMel <melissamwaltner@gmail.com>
2023-12-04 13:21:33 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -12,27 +12,3 @@ define('IS_VITE_DEVELOPMENT', true);
 
 
 include "inc/inc.vite.php";
-
-// add_action( 'wp_enqueue_scripts', 'theme_slug_enqueue_scripts' );
-
-// function theme_slug_enqueue_scripts() {
-// 	wp_enqueue_script( 
-// 		'theme-js',
-// 		get_parent_theme_file_uri( 'main.js' ),
-// 		array(),
-// 		wp_get_theme()->get( 'Version' ),
-// 		true
-// 	);
-// }
-
-// add_filter('script_loader_tag', 'add_type_attribute' , 10, 3);
-
-// function add_type_attribute($tag, $handle, $src) {
-//   // if not your script, do nothing and return original $tag
-//   if ( 'theme-js' !== $handle ) {
-//       return $tag;
-//   }
-//   // change the script tag by adding type="module" and return it.
-//   $tag = '<script type="module" src="' . esc_url( $src ) . '"></script>';
-//   return $tag;
-// }
