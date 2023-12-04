2b00fc7b7174074e277a18e8ab863db1563a51fd
Melissa <melissamwaltner@gmail.com>
2023-12-04 15:25:33 -0600

    Tab formatting

--- blocks/register-blocks.php
+++ blocks/register-blocks.php
@@ -7,13 +7,13 @@
  * @link https://developer.wordpress.org/reference/hooks/init/
  */
 function register_acf_blocks() {
-    /**
-     * We register our block's with WordPress's handy
-     * register_block_type();
-     *
-     * @link https://developer.wordpress.org/reference/functions/register_block_type/
-     */
-    // register_block_type( __DIR__ . '/team-member' );
+  /**
+   * We register our block's with WordPress's handy
+   * register_block_type();
+   *
+   * @link https://developer.wordpress.org/reference/functions/register_block_type/
+   */
+  // register_block_type( __DIR__ . '/team-member' );
 }
 // Here we call our register_acf_block() function on init.
 add_action( 'init', 'register_acf_blocks' );
--- inc/inc.vite.php
+++ inc/inc.vite.php
@@ -2,7 +2,7 @@
 
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) )
-    exit;  
+  exit;  
     
 /*
  * VITE & Tailwind JIT development
@@ -26,63 +26,57 @@ define('VITE_SERVER', 'http://localhost:3000');
 define('VITE_ENTRY_POINT', '/main.js');
 
 function vite_head_module_hook() {
-    echo '<script type="module" crossorigin src="' . VITE_SERVER . VITE_ENTRY_POINT . '"></script>';
+  echo '<script type="module" crossorigin src="' . VITE_SERVER . VITE_ENTRY_POINT . '"></script>';
 }
 
 function manifest_files() {
-    // production version, 'npm run build' must be executed in order to generate assets
-        // ----------
+  // production version, 'npm run build' must be executed in order to generate assets
+  // ----------
 
-        // read manifest.json to figure out what to enqueue
-        $manifest = json_decode( file_get_contents( DIST_PATH . '/.vite/manifest.json'), true );
+  // read manifest.json to figure out what to enqueue
+  $manifest = json_decode( file_get_contents( DIST_PATH . '/.vite/manifest.json'), true );
+  
+  // is ok
+  if (is_array($manifest)) {
+      
+    // get first key, by default is 'main.js' but it can change
+    $manifest_key = array_keys($manifest);
+    if (isset($manifest_key[0])) {
         
-        // is ok
-        if (is_array($manifest)) {
-            
-            // get first key, by default is 'main.js' but it can change
-            $manifest_key = array_keys($manifest);
-            if (isset($manifest_key[0])) {
-                
-                // enqueue CSS files
-                foreach(@$manifest[$manifest_key[0]]['css'] as $css_file) {
-                    wp_enqueue_style( 'main', DIST_URI . '/' . $css_file );
-                }
-                
-                // enqueue main JS file
-                $js_file = @$manifest[$manifest_key[0]]['file'];
-                if ( ! empty($js_file)) {
-                    wp_enqueue_script( 'main', DIST_URI . '/' . $js_file, JS_DEPENDENCY, '', JS_LOAD_IN_FOOTER );
-                }
-                
-            }
+        // enqueue CSS files
+      foreach(@$manifest[$manifest_key[0]]['css'] as $css_file) {
+          wp_enqueue_style( 'main', DIST_URI . '/' . $css_file );
+      }
+      
+      // enqueue main JS file
+      $js_file = @$manifest[$manifest_key[0]]['file'];
+      if ( ! empty($js_file)) {
+          wp_enqueue_script( 'main', DIST_URI . '/' . $js_file, JS_DEPENDENCY, '', JS_LOAD_IN_FOOTER );
+      }
+        
+    }
 
-        }
+  }
 }
 
 // enqueue hook
 add_action( 'wp_enqueue_scripts', function() {
     
-    if (defined('IS_VITE_DEVELOPMENT') && IS_VITE_DEVELOPMENT === true) {
+  if (defined('IS_VITE_DEVELOPMENT') && IS_VITE_DEVELOPMENT === true) {
+    // insert hmr into head for live reload
+    add_action('wp_head', 'vite_head_module_hook');
 
-        // insert hmr into head for live reload
-        
-        add_action('wp_head', 'vite_head_module_hook');
-
-    } else {
-
-        manifest_files();
-
-    }
-
-
+  } else {
+    manifest_files();
+  }
 });
 
 function my_theme_editor_enqueue()
 {
-    if (defined('IS_VITE_DEVELOPMENT') && IS_VITE_DEVELOPMENT === true) {
-        vite_head_module_hook();
-    } else {
-        manifest_files();
-    }
+  if (defined('IS_VITE_DEVELOPMENT') && IS_VITE_DEVELOPMENT === true) {
+    vite_head_module_hook();
+  } else {
+    manifest_files();
+  }
 }
 add_action('enqueue_block_editor_assets', 'my_theme_editor_enqueue');
